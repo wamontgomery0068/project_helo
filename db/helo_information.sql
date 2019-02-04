7e20ce554aea1d66b6c559f50d0c569673f8be9a
Your Name <wamontgomery0068@gmail.com>
2019-02-04 15:12:36 -0600

    Fixed Table

--- db/helo_information.sql
+++ db/helo_information.sql
@@ -1,7 +1,9 @@
 CREATE TABLE helo_information
 (
   id SERIAL PRIMARY KEY,
+  username VARCHAR (500),
   title VARCHAR (100) UNIQUE NOT NULL,
+  profile_image VARCHAR (500) NOT NULL,
   image_url VARCHAR (500) NOT NULL,
   content VARCHAR (500)
 );
