88071e652622f919eefa7a0b29718ddc12b64b4c
Your Name <wamontgomery0068@gmail.com>
2019-02-05 11:21:46 -0600

    Filled in the reducer

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -9,6 +9,8 @@ const initialState = {
 // Action Types
 const Get_Information = "Information_Data";
 const LOGIN = "LOGIN";
+const REGISTER = "REGISTER";
+const GET_USER = "GET_USER";
 
 // Get_Information Action Creator
 export function getInformation(){
@@ -26,11 +28,37 @@ export function login(username, password){
     };
 };
 
+// Register Action Creator
+export function register(username, password){
+    return{
+        type: REGISTER,
+        payload: axios.post("/auth/register", { username, password })
+    };
+};
+
+// GET_USER Action Creator
+export function getUser(){
+    return{
+        type: GET_USER,
+        payload: axios.get("/auth/user")
+    };
+};
+
 // Reducer Function
 export default function reducer(state = initialState, action){
     switch (action.type){
         case Get_Information + "_FULFILLED":
             return { ...state, user:action.payload.data};
+        case LOGIN + "_FULFILLED":
+            return { ...state, user: action.payload.data };
+        case LOGIN + "_REJECTED":
+            return { ...state, error: "Bad login" };
+        case REGISTER + "_FULFILLED":
+            return { ...state, user: action.payload.data };
+        case REGISTER + "_REJECTED":
+            return { ...state, error: "Bad Register" };
+        case GET_USER + "_FULFILLED":
+            return { ...state, user: action.payload.data };
         default:
             return state;
     };
